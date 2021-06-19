8cc9baa84d09b369dde85d37b38647f5bc1daf37
Potap <55599818+PotapIlya@users.noreply.github.com>
2021-06-19 19:56:03 +0300

    12

--- assets/build/js/app.js
+++ assets/build/js/app.js
@@ -17,12 +17,22 @@ class Transform
         this.shadowX = document.querySelector('#shadowX');
         this.shadowY = document.querySelector('#shadowY');
         this.shadowF = document.querySelector('#shadowF');
+        this.shadowColor = document.querySelectorAll('.shadow-color>div');
 
-        this.shadowArray = [
-            this.shadowX,
-            this.shadowY,
-            this.shadowF
-        ];
+
+        this.skewX = document.querySelector('#skew');
+
+        this.getCode = document.querySelector('#getCode');
+        this.getCodeForm = document.querySelector('#getCodeForm');
+        this.resCode = {};
+
+
+        this.shadowArray = {
+            x: 0,
+            y: 0,
+            f: 0,
+            color: '#fff',
+        }
 
         this.init();
     }
@@ -31,6 +41,12 @@ class Transform
         this.changeWidth();
         this.changeHeight();
         this.rotateFunct();
+        this.colorFigure();
+        this.borderRadius();
+        this.getCodeFunct();
+        this.skew();
+        this.boxShadow();
+
     }
 
     changeWidth(){
@@ -50,17 +66,94 @@ class Transform
 
     changeNumberSize(value){
         if (value.width){
-            this.widthSize.innerText = value.width
+            this.widthSize.innerText = value.width;
+            this.resCode.width = value.width;
         }
         if (value.height){
             this.heightSize.innerText = value.height;
+            this.resCode.height = value.height;
         }
     }
 
     rotateFunct(){
         this.rotate.addEventListener('mousemove', (e) => {
-            this.figure.style.transform = 'rotate(' + rotateVal + 'deg )';
+            let value = e.target.value;
+            this.figure.style.transform = 'rotate(' + value + 'deg )';
+            this.changeCenterText(value + 'deg');
+            this.resCode.rotate = value;
+            this.resCode.skewX = 0;
         });
+    }
+
+    colorFigure(){
+        this.colorBox.forEach( item =>{
+            item.addEventListener('click', () =>{
+                this.figure.style.background = item.dataset.value;
+                this.changeCenterText(item.dataset.color)
+                this.resCode.background = item.dataset.value;
+            });
+        });
+    }
+
+    changeCenterText(data){
+        this.number.textContent = data;
+    }
+
+    borderRadius(){
+        this.rounding.addEventListener('mousemove', (e) => {
+            const value = e.target.value+'px';
+            this.figure.style.borderRadius = value;
+            this.resCode.borderRadius = value;
+
+            this.changeCenterText(value);
+        })
+    }
+
+    skew(){
+        this.skewX.addEventListener('mousemove', (e) => {
+            let value = e.target.value;
+            this.figure.style.transform = `skewX(${value}deg)`;
+            this.changeCenterText(value);
+            this.resCode.skewX = value;
+            this.resCode.rotate = 0;
+        })
+    }
+
+    getCodeFunct(){
+        this.getCode.addEventListener('click', () => {
+            this.getCodeForm.innerText = Object.entries(this.resCode).reduce((prev, item) => {
+                return `${(prev + item).split(',').join(':')};\n`;
+            }, '');
+        })
+    }
+
+    boxShadow(){
+        this.shadowX.addEventListener('mousemove', (e) => {
+            this.shadowArray.x = e.target.value;
+            this.changeBoxShadow();
+            this.changeCenterText(e.target.value);
+        })
+        this.shadowY.addEventListener('mousemove', (e) => {
+            this.shadowArray.y = e.target.value;
+            this.changeBoxShadow();
+            this.changeCenterText(e.target.value);
+        })
+        this.shadowF.addEventListener('mousemove', (e) => {
+            this.shadowArray.f = e.target.value;
+            this.changeBoxShadow();
+            this.changeCenterText(e.target.value);
+        })
+        this.shadowColor.forEach(item => {
+            item.addEventListener('click', (e) => {
+                this.shadowArray.color = e.target.dataset.value;
+                this.changeBoxShadow();
+                this.changeCenterText(e.target.dataset.color);
+            })
+        })
+
+    }
+    changeBoxShadow(){
+        this.figure.style.boxShadow = `${this.shadowArray.x}px ${this.shadowArray.y}px ${this.shadowArray.f}px ${this.shadowArray.color}`
     }
 
 }
--- assets/build/preAssets/js/scripts.js
+++ assets/build/preAssets/js/scripts.js
@@ -22,7 +22,17 @@ var Transform = /*#__PURE__*/function () {
     this.shadowX = document.querySelector('#shadowX');
     this.shadowY = document.querySelector('#shadowY');
     this.shadowF = document.querySelector('#shadowF');
-    this.shadowArray = [this.shadowX, this.shadowY, this.shadowF];
+    this.shadowColor = document.querySelectorAll('.shadow-color>div');
+    this.skewX = document.querySelector('#skew');
+    this.getCode = document.querySelector('#getCode');
+    this.getCodeForm = document.querySelector('#getCodeForm');
+    this.resCode = {};
+    this.shadowArray = {
+      x: 0,
+      y: 0,
+      f: 0,
+      color: '#fff'
+    };
     this.init();
   }
 
@@ -32,6 +42,11 @@ var Transform = /*#__PURE__*/function () {
       this.changeWidth();
       this.changeHeight();
       this.rotateFunct();
+      this.colorFigure();
+      this.borderRadius();
+      this.getCodeFunct();
+      this.skew();
+      this.boxShadow();
     }
   }, {
     key: "changeWidth",
@@ -66,10 +81,12 @@ var Transform = /*#__PURE__*/function () {
     value: function changeNumberSize(value) {
       if (value.width) {
         this.widthSize.innerText = value.width;
+        this.resCode.width = value.width;
       }
 
       if (value.height) {
         this.heightSize.innerText = value.height;
+        this.resCode.height = value.height;
       }
     }
   }, {
@@ -78,8 +95,114 @@ var Transform = /*#__PURE__*/function () {
       var _this3 = this;
 
       this.rotate.addEventListener('mousemove', function (e) {
-        _this3.figure.style.transform = 'rotate(' + rotateVal + 'deg )';
+        var value = e.target.value;
+        _this3.figure.style.transform = 'rotate(' + value + 'deg )';
+
+        _this3.changeCenterText(value + 'deg');
+
+        _this3.resCode.rotate = value;
+        _this3.resCode.skewX = 0;
       });
+    }
+  }, {
+    key: "colorFigure",
+    value: function colorFigure() {
+      var _this4 = this;
+
+      this.colorBox.forEach(function (item) {
+        item.addEventListener('click', function () {
+          _this4.figure.style.background = item.dataset.value;
+
+          _this4.changeCenterText(item.dataset.color);
+
+          _this4.resCode.background = item.dataset.value;
+        });
+      });
+    }
+  }, {
+    key: "changeCenterText",
+    value: function changeCenterText(data) {
+      this.number.textContent = data;
+    }
+  }, {
+    key: "borderRadius",
+    value: function borderRadius() {
+      var _this5 = this;
+
+      this.rounding.addEventListener('mousemove', function (e) {
+        var value = e.target.value + 'px';
+        _this5.figure.style.borderRadius = value;
+        _this5.resCode.borderRadius = value;
+
+        _this5.changeCenterText(value);
+      });
+    }
+  }, {
+    key: "skew",
+    value: function skew() {
+      var _this6 = this;
+
+      this.skewX.addEventListener('mousemove', function (e) {
+        var value = e.target.value;
+        _this6.figure.style.transform = "skewX(".concat(value, "deg)");
+
+        _this6.changeCenterText(value);
+
+        _this6.resCode.skewX = value;
+        _this6.resCode.rotate = 0;
+      });
+    }
+  }, {
+    key: "getCodeFunct",
+    value: function getCodeFunct() {
+      var _this7 = this;
+
+      this.getCode.addEventListener('click', function () {
+        _this7.getCodeForm.innerText = Object.entries(_this7.resCode).reduce(function (prev, item) {
+          return "".concat((prev + item).split(',').join(':'), ";\n");
+        }, '');
+      });
+    }
+  }, {
+    key: "boxShadow",
+    value: function boxShadow() {
+      var _this8 = this;
+
+      this.shadowX.addEventListener('mousemove', function (e) {
+        _this8.shadowArray.x = e.target.value;
+
+        _this8.changeBoxShadow();
+
+        _this8.changeCenterText(e.target.value);
+      });
+      this.shadowY.addEventListener('mousemove', function (e) {
+        _this8.shadowArray.y = e.target.value;
+
+        _this8.changeBoxShadow();
+
+        _this8.changeCenterText(e.target.value);
+      });
+      this.shadowF.addEventListener('mousemove', function (e) {
+        _this8.shadowArray.f = e.target.value;
+
+        _this8.changeBoxShadow();
+
+        _this8.changeCenterText(e.target.value);
+      });
+      this.shadowColor.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+          _this8.shadowArray.color = e.target.dataset.value;
+
+          _this8.changeBoxShadow();
+
+          _this8.changeCenterText(e.target.dataset.color);
+        });
+      });
+    }
+  }, {
+    key: "changeBoxShadow",
+    value: function changeBoxShadow() {
+      this.figure.style.boxShadow = "".concat(this.shadowArray.x, "px ").concat(this.shadowArray.y, "px ").concat(this.shadowArray.f, "px ").concat(this.shadowArray.color);
     }
   }]);
 
